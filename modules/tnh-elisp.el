bfc3a23d3c27d932a268dd1773e69c1b32375feb
TheNerdyHamster <leo@letnh.com>
2024-10-12 17:52:29 +0200

    Changed elisp-format to elisp-autofmt

--- modules/tnh-elisp.el
+++ modules/tnh-elisp.el
@@ -1,6 +1,9 @@
-(eval-when-compile (require 'use-package))
+(eval-when-compile
+  (require 'use-package))
 
-(use-package elisp-format
-  :ensure t)
+(use-package elisp-autofmt
+  :ensure t
+  :hook (emacs-lisp-mode . elisp-autofmt-mode)
+  :config (setq elisp-autofmt-load-packages-local '("use-package")))
 
 (provide 'tnh-elisp)
