1d513176f7621764337a4b6003e588532c6155cc
TheNerdyHamster <leo@letnh.com>
2024-10-12 17:53:00 +0200

    Added magit delta and formatted file

--- modules/tnh-git.el
+++ modules/tnh-git.el
@@ -1,16 +1,24 @@
-(eval-when-compile (require 'use-package))
+(eval-when-compile
+  (require 'use-package))
 
 (use-package magit
   :ensure t
-  :bind (("C-M-;" . magit-status-here)
-	 ("C-c C-g" . magit-status-here)))
+  :bind (("C-M-;" . magit-status-here) ("C-c C-g" . magit-status-here)))
 
 (use-package git-modes
   :ensure t)
 
+(use-package magit-delta
+  :ensure t
+  :custom
+  (magit-delta-default-dark-theme "gruvbox-dark")
+  :hook (magit-mode . magit-delta-mode))
+
 (use-package git-gutter
   :ensure t
-  :bind (("C-c [" . git-gutter:next-hunk)
-	 ("C-c ]" . git-gutter:previous-hunk))
-  :config
-  (global-git-gutter-mode))
+  :bind
+  (("C-c [" . git-gutter:next-hunk)
+   ("C-c ]" . git-gutter:previous-hunk))
+  :config (global-git-gutter-mode))
+
+(provide 'tnh-git)
