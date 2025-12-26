74a33e51595476f06e641b5568b8ca3d70a504fa
TheNerdyHamster <leo@letnh.com>
2025-12-26 16:23:12 +0100

    feat: Added basic ibuffer stuff

---
+++ modules/tnh-ibuffer.el
@@ -0,0 +1,10 @@
+;;; tnh-ibuffer.el --- Basic emacs ibuffer configuration -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(global-set-key [remap list-buffers] 'ibuffer)
+
+(provide 'tnh-ibuffer)
+
+;;; tnh-ibuffer.el end here
+
