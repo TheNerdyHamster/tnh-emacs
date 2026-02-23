cf8f76589c25965ec8489f58c8d677b30e8bec6f
TheNerdyHamster <leo@letnh.com>
2026-02-23 21:03:20 +0100

    feat: Fixed font and update binding for code actions

--- modules/tnh-eglot.el
+++ modules/tnh-eglot.el
@@ -1,6 +1,6 @@
 (use-package eglot
   :bind (:map eglot-mode-map
-	      ("C-c C-a" . eglot-code-action)
+	      ("C-c C-a" . eglot-code-actions)
 	      ("C-c C-r" . eglot-rename))
   :config
   (setq eglot-autoshutdown t))
--- modules/tnh-fonts.el
+++ modules/tnh-fonts.el
@@ -2,19 +2,17 @@
 ;;; Commentary:
 ;;; Code:
 (set-face-attribute 'default nil
-  :family "Iosevka Nerd Font"
+  :family "Iosevka Nerd Font Mono"
   :height 140
   :weight 'normal
   :width 'normal)
 
-;; Also set the 'fixed-pitch' face for consistency in code buffers
 (set-face-attribute 'fixed-pitch nil
-  :family "Iosevka Nerd Font"
+  :family "Iosevka Nerd Font Mono"
   :height 140
   :weight 'normal
   :width 'normal)
 
-;; Set a different, more readable font for prose
 (set-face-attribute 'variable-pitch nil
   :family "SF Pro" ;; Or any other proportional font you like
   :height 150)
