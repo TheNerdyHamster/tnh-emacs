4ad341af3d480e8ad8d500631a734380bf4ad1b2
TheNerdyHamster <leo@letnh.com>
2026-06-03 13:41:25 +0200

    feat: Updated go package

--- modules/tnh-go.el
+++ modules/tnh-go.el
@@ -3,13 +3,14 @@
   :mode ("\\.go\\'" . go-mode)
   :hook ((before-save . gofmt-before-save)
          (go-mode . eglot-ensure)
-	 (go-mode . (lambda ()
-		      (setq tab-width 4)
-		      (setq indent-tabs-mode t))))
+         (go-mode . (lambda ()
+                      (setq-local tab-width 4)
+                      (setq-local indent-tabs-mode t))))
   :custom
   (gofmt-command "gofumpt")
+  :config
   (with-eval-after-load 'eglot
-    (add-to-list 'eglot-server-programs '(go-ts-mode . ("gopls")))))
+    (add-to-list 'eglot-server-programs '((go-mode go-ts-mode) . ("gopls")))))
 
 (use-package go-eldoc
   :ensure t
